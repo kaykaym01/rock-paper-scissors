604f0356abb34836a7d88d54920a6d06d437a8a7
kaymoore <kaymoore@macbook>
2022-04-22 11:24:03 -0400

    Remove method that required 5 rounds

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
- // method for computer to randomly select rock paper or scissors
- function computerPlay() {
+// method for computer to randomly select rock paper or scissors
+function computerPlay() {
     const moves = ["rock", "paper", "scissors"];
     let randomIndex = Math.floor(Math.random() * moves.length);
     let randomChoice = moves[randomIndex];
@@ -8,7 +8,7 @@
 
 // method that takes click event and player move, randomly selects computer move, and returns 1 for a user win, -1 for
 // a user loss, and 0 for a draw/tie
-function playRound(playerSelection){
+function playRound(playerSelection) {
     let computerSelection = computerPlay();
 
     console.log(`You: ${playerSelection}`);
@@ -21,16 +21,16 @@ function playRound(playerSelection){
     ]);
 
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == computerSelection){
+    if (playerSelection == computerSelection) {
         return 0;
-    } else if (winningMoves.get(playerSelection) == computerSelection){
+    } else if (winningMoves.get(playerSelection) == computerSelection) {
         return 1;
     } else {
         return -1;
     }
 }
 
-function playGame(e){
+function playGame(e) {
     let playerMove = this.getAttribute("data-value");
     let roundResult = playRound(playerMove);
     displayRoundResult(roundResult);
@@ -41,12 +41,12 @@ playButtons.forEach(button => button.addEventListener('click', playGame));
 
 // method that takes in integer result and if 0, displays draw,
 // if 1 displays win, and if -1 displays loss
-function displayRoundResult(result){
-    switch(result){
+function displayRoundResult(result) {
+    switch (result) {
         case -1:
             console.log("You Lose");
             break;
-        case 0: 
+        case 0:
             console.log("Draw");
             break;
         case 1:
@@ -54,29 +54,9 @@ function displayRoundResult(result){
     }
 }
 
-// method that plays 5 rounds between player and computer
-// and displays results of each round and winner at the end
-function game(){
-    let score = 0;
-    for (let i = 0; i < 5; i++){
-        let playerMove = readAndValidateInput();
-        if (playerMove == null){
-            console.log("Game Cancelled.");
-            return;
-        }
-        let result = playRound(playerMove);
-        displayRoundResult(result);
-        if (result == 1){
-            score++;
-        }
-        console.log("");
-    }
-    displayGameResult(score);
-}
-
 // method to read int score and if score >= 3, displays win
 // else displays loss
-function displayGameResult(score){
+function displayGameResult(score) {
     console.log(`Game Score: ${score}/5`);
     let gameResult = (score >= 3) ? "win" : "lose";
     console.log(`Game Result: You ${gameResult}!`);
